835a227816aa65ff4ad3a840b0bf5e4587151361
10sr <8slashes+git@gmail.com>
2017-03-14 13:06:37 +0900

    Add __str__ method to model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ class TimeOfSleep(models.Model):
     time_of_sleep = models.DateTimeField("Time Of Sleep")
     datetime = models.DateTimeField("Time when record inserted")
     id_str = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "id_str: {}, tos: {}, record time: {}".format(
+            self.id_str, self.time_of_sleep, self.datetime
+        )
