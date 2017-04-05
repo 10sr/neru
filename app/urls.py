0c1bdd357a0a0228f5341a7c3ac1110b52f3173e
10sr <8slashes+git@gmail.com>
2017-04-05 18:26:41 +0900

    WIP
    https://docs.djangoproject.com/en/1.11/intro/tutorial03/#write-views-that-actually-do-something

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 
 urlpatterns = [
     url(r"^$", views.index, name="index"),
+    # TODO: Fix URL to use
+    url(r"^user/(?P<user_id>[_0-9a-zA-Z]+)$", views.user, name="user")
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hell, world!")
+
+
+def user(request, user_id):
+    return (HttpResponse("history of {}".format(user_id)))
