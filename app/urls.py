7f22f5462194bee8ced3e795b19c53ba9cf2010b
10sr <8slashes+git@gmail.com>
2017-04-07 17:43:36 +0900

    Add form

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,7 @@ app_name = "app"
 urlpatterns = [
     url(r"^$", views.index, name="index"),
     # TODO: Fix URL to use
-    url(r"^user/(?P<username>[_0-9a-zA-Z]+)$", views.user, name="user")
+    url(r"^user/(?P<username>[_0-9a-zA-Z]+)$", views.user, name="user"),
+    url(r"^user/(?P<username>[_0-9a-zA-Z]+)/post$", views.user_post,
+        name="user_post")
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse, Http404
+import datetime
+
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.utils import timezone
+from django.urls import reverse
 
 from app import models
 
@@ -15,6 +19,42 @@ def user(request, username):
     template = loader.get_template("app/index.html")
     return HttpResponse(
         template.render({
-            "sleeps": sleeps
+            "sleeps": sleeps,
+            "username": username
         }, request)
     )
+
+
+def user_post(request, username):
+    try:
+        posted_time_of_sleep = request.POST["time_of_sleep"]
+    except KeyError:
+        template = loader.get_template("app/index.html")
+        return HttpResponse(
+            template.render({
+                "sleeps": models.TimeOfSleep.objects.filter(username=username),
+                "error_message": "time_of_sleep not given",
+                "username": username
+            })
+        )
+
+    try:
+        time_of_sleep = datetime.datetime.strptime(posted_time_of_sleep,
+                                                   "%Y%m%d")
+    except ValueError:
+        template = loader.get_template("app/index.html")
+        return HttpResponse(
+            template.render({
+                "sleeps": models.TimeOfSleep.objects.filter(username=username),
+                "error_message": "invalid time_of_sleep format",
+                "username": username
+            })
+        )
+
+    models.TimeOfSleep(
+        username=username,
+        id_str="",
+        datetime=timezone.now(),
+        time_of_sleep=time_of_sleep
+    ).save
+    return HttpResponseRedirect(reverse("app:user", args=(username,)))
