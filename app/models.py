4389cd28d64dd15316fabe78120801785c0575b4
10sr <8.slashes@gmail.com>
2018-10-26 13:36:59 +0900

    Add TwitterUsername model

--- app/models.py
+++ app/models.py
@@ -4,10 +4,21 @@ from django.db import models
 class TimeOfSleep(models.Model):
     time_of_sleep = models.DateTimeField("Time Of Sleep")
     datetime = models.DateTimeField("Time when record inserted")
+    # Twitter id in number
     id_str = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "id_str: {}, tos: {}, record time: {}".format(
+            self.id_str, self.time_of_sleep, self.datetime
+        )
+
+class TwitterUsername(models.Model):
+    # Twitter id in number
+    id_str = models.CharField(max_length=50)
+    # Twitter screen name
     username = models.CharField(max_length=20)
 
     def __str__(self):
-        return "id_str: {}, username: {}, tos: {}, record time: {}".format(
-            self.id_str, self.username, self.time_of_sleep, self.datetime
+        return "id_str: {}, username: {}".format(
+            self.id_str, self.username
         )
