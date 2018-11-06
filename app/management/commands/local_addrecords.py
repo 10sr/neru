5835b72db32bac424967f0c35a50e4dedd06f61e
10sr <8.slashes@gmail.com>
2018-11-06 15:48:32 +0900

    Add local_addrecords

---
+++ app/management/commands/local_addrecords.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from app import models
+
+_twitteruser_username = "10sr"
+_twitteruser_id_str = "73722749"
+
+
+class Command(BaseCommand):
+    help = "Count sleep record num"
+
+    def add_arguments(self, parser):
+        return
+
+    def handle(self, *args, **kargs):
+        try:
+            user = models.TwitterUser.objects.get(username=_twitteruser_username)
+            print("TwitterUser `{}' already exists".format(user))
+        except models.TwitterUser.DoesNotExist as e:
+            print("TwitterUser {} not exists, creating".format(_twitteruser_username))
+            models.TwitterUser(
+                username=_twitteruser_username,
+                id_str=_twitteruser_id_str
+            ).save()
+        return
