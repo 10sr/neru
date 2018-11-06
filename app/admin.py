2c2193ff4f8c8a3d5374e0b159a80388f7916e63
10sr <8.slashes@gmail.com>
2018-11-06 13:47:00 +0900

    Upddate admin

--- app/admin.py
+++ app/admin.py
@@ -5,12 +5,28 @@ from django.db.models import Model
 
 import app.models
 
-# Register all models to admin page
-# https://docs.djangoproject.com/en/1.10/intro/tutorial02/#make-the-poll-app-modifiable-in-the-admin
-for key in dir(app.models):
-    e = getattr(app.models, key)
-    if inspect.isclass(e) and issubclass(e, Model):
-        admin.site.register(e)
 
-# admin.site.register(TimeOfSleep)
-# admin.site.register(TwitterUsername)
+# Configure site header
+admin.AdminSite.site_header = "Neru Admin"
+
+# # Register all models to admin page
+# # https://docs.djangoproject.com/en/1.10/intro/tutorial02/#make-the-poll-app-modifiable-in-the-admin
+# for key in dir(app.models):
+#     e = getattr(app.models, key)
+#     if inspect.isclass(e) and issubclass(e, Model):
+#         admin.site.register(e)
+
+class _TimeOfSleepAdmin(admin.ModelAdmin):
+    # fields = ["id_str", "time_of_sleep", "note", "datetime"]
+    fieldsets = [
+        (None, {
+            "fields": ["id_str", "datetime"]
+        }),
+        ("Neru info", {
+            "fields": ["time_of_sleep", "note"]
+        }),
+    ]
+
+
+admin.site.register(app.models.TimeOfSleep, _TimeOfSleepAdmin)
+admin.site.register(app.models.TwitterUser)
